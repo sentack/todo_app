c1b373d5064247a9f97c034c4c3ddc2d77507d67
sentack <sentak16@gmail.com>
2025-09-27 13:12:54 +0300

    another lint error fix

--- app/settings/page.tsx
+++ app/settings/page.tsx
@@ -21,6 +21,7 @@ export default function SettingsPage() {
 
   useEffect(() => {
     checkAuth()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
   const checkAuth = async () => {
